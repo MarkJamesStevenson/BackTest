b8e92825d89735ffe726b4d86a168900691549f6
Mark Stevenson <markjamesstevenson@gmail.com>
2017-02-11 15:49:50 +0000

    start of event loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include "BackTester/eventqueue.h"
+#include "BackTester/ievent.h"
+#include "BackTester/marketevent.h"
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     EventQueue eventQueue;
 
-    std::cout << "HI" << eventQueue.IsEmpty() << std::endl;
+    eventQueue.AddEvent(std::make_unique<MarketEvent>());
+    if (!eventQueue.IsEmpty())
+    {
+        std::unique_ptr<IEvent> event = std::move(eventQueue.GetNextEvent());
+        switch (event->getEventType()){
+            case IEvent::Event_Type::MARKET_EVENT :
+                std::cout << "market event" << std::endl;
+                break;
+            default :
+                std::cout << "do not recognise the event type" << std::endl;
+                break;
+        }
+    }
 }
 
