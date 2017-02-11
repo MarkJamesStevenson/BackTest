3cba6ce534adbe708a5f2b0368f9289625a1df38
Mark Stevenson <markjamesstevenson@gmail.com>
2017-02-11 02:54:27 +0000

    Add the test executable

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
-#include <gtest/gtest.h>
-
-using namespace std;
-
-double square_root (const double x)
-{
-    return x*x;
-}
-
-
-TEST (SquareRootTest, PositiveNos) {
-    EXPECT_EQ (18.0, square_root (324.0));
-    EXPECT_EQ (25.4, square_root (645.16));
-    EXPECT_EQ (50.3321, square_root (2533.310224));
-}
-
-TEST (SquareRootTest, ZeroAndNegativeNos) {
-    ASSERT_EQ (0.0, square_root (0.0));
-    ASSERT_EQ (-1, square_root (-22.0));
-}
 
 int main(int argc, char *argv[])
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    std::cout << "logic in here" << std::endl;
 }
 
