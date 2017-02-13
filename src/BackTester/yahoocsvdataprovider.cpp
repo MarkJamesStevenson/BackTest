ae111887f6795c9c23f1b93b577a0f62e4207e9b
Mark Stevenson <markjamesstevenson@gmail.com>
2017-02-13 00:04:33 +0000

    Get curl working

--- src/BackTester/yahoocsvdataprovider.cpp
+++ src/BackTester/yahoocsvdataprovider.cpp
@@ -1,12 +1,63 @@
 #include "yahoocsvdataprovider.h"
 #include <iostream>
+#include "curl/curl.h"
+#include <string>
+
+using namespace std;
 
 void YahooCSVDataProvider::DownloadData()
 {
     std::cout << "Downloading the data to disk" << std::endl;
+    UrlParser("hi", 1, 2, 3, 4, 5, 6, 'A');
 }
 
 void YahooCSVDataProvider::UpdateBars()
 {
 
+}
+size_t my_write_func(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+    return fwrite(ptr, size, nmemb, stream);
+}
+
+bool YahooCSVDataProvider::UrlParser(const string& ID, const int& fromMonth, const int& fromDay, const int& fromYear, const int& toMonth, const int& toDay, const int& toYear, char tradingPeriod)
+{
+    curl = curl_easy_init();
+    if (curl) {
+        FILE* my_file;
+        errno_t err;
+        const char* file_name = "archive\\test.txt";
+
+        if ((err = fopen_s(&my_file, file_name, "w")) != 0)
+        {
+            perror("Error opening file");
+            return false;
+        }
+        else
+        {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://real-chart.finance.yahoo.com/table.csv?s=VOD.L&a=0&b=1&c=2016&d=4&e=9&f=2016&g=d&ignore=.csv");
+
+            //send the data to the memory function
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_write_func);
+
+            //pass chunk struct to the callback function
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, my_file);
+
+            result = curl_easy_perform(curl);
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+
+            if (result == CURLE_OK)
+            {
+                std::cout << "Successfully parsed data to " << std::endl;
+            }
+            else
+            {
+                std::cout << "Error parsing data." << result << std::endl;
+                return false;
+            }
+        }
+    }
+    return true;
 }
--- src/BackTester/yahoocsvdataprovider.h
+++ src/BackTester/yahoocsvdataprovider.h
@@ -2,15 +2,25 @@
 #define YAHOOCSVDATAPROVIDER_H
 
 #include "dataprovider.h"
+#include <string>
+#include "curl/curl.h"
 
 class YahooCSVDataProvider : public DataProvider
 {
 public:
     YahooCSVDataProvider() = default;
 
+    bool UrlParser(const std::string& ID, const int& fromMonth, const int& fromDay, const int& fromYear, const int& toMonth, const int& toDay, const int& toYear, char tradingPeriod);
+
     void DownloadData() override;
 
     void UpdateBars() override;
+
+private:
+    CURL *curl;
+    CURLcode result;
 };
+
+size_t my_write_func(void *ptr, size_t size, size_t nmemb, FILE *stream);
 
 #endif // YAHOOCSVDATAPROVIDER_H
